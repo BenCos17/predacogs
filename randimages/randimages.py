28091b088803f7461d7ca53adf64aa071f1648a2
ben <52817096+BenCos17@users.noreply.github.com>
2024-09-26 12:39:31 +0100

    Update randimages.py

--- randimages/randimages.py
+++ randimages/randimages.py
@@ -2,6 +2,7 @@ import praw
 import random
 from redbot.core import commands, checks, Config
 from redbot.core.i18n import Translator, cog_i18n
+import discord
 
 from .core import Core
 from . import constants as sub
